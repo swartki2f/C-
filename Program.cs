2f952d17365ecf2b90943fa20a8998bc93940be6
swartki2fjava <sawrtdelatore@gmail.com>
2025-03-18 10:38:33 +0100

    version 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MyProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("debut du prog");
+        
+
+        Console.WriteLine("saisir votre nom   ");
+        string nom= Console.ReadLine();
+
+        Console.WriteLine("saisir votre age  ");
+        string age= Console.ReadLine();
+        Console.WriteLine("je suis "+nom+" j'ai "+age+"ans");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        System.Console.WriteLine("Fin du programme");
+    }
+}
